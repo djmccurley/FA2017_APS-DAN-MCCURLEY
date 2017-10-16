2fb4ea6f60f5fbaf1e7e75f0475decb7f872f4aa
Dan McCurley <djmccurley@gmail.com>
2017-10-15 23:16:55 -0500

    added menu screen

--- js/menu.js
+++ js/menu.js
@@ -5,9 +5,15 @@ Aftermath.Menu.prototype = {
 		this.background = this.game.add.tileSprite(0, 0, this.game.width, this.game.height, 'sand');
 		this.background.autoScroll(-10, 0);
 
-		var text = "Press Spacebar to begin";
+		var text = "Aftermath '03 (Alpha) - Press Spacebar to begin";
 		var style = {font: "2rem Helvetica, Arial, Sans-Serif", fill: "#fff", align: "center"};
 		var t = this.game.add.text(this.game.width/2, this.game.height/2, text, style);
 		t.anchor.set(0.5);
 	}, 
-}
+
+	update: function(){
+		if (this.game.input.keyboard.justPressed(Phaser.Keyboard.SPACEBAR)) {
+			this.state.start('Game');
+		}
+	}
+};
